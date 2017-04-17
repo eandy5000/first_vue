c677dc763a26de90929a3ef88ffa4dd0a0626dbf
Andy Eccleston <eandy5000@yahoo.com>
2017-04-17 09:18:53 -0500

    basic vue instance running

--- public/script.js
+++ public/script.js
@@ -1,7 +1,20 @@
 new Vue({
     el: '#app',
+    methods: {
+        addToCart: function() {
+            console.log(cart)
+            return this.total += this.price;
+        }
+    },
     data: {
         total: 0,
+        price: 9.99,
+        products: [
+            {id: 1, name: 'one'},
+            {id: 2, name: 'two'},
+            {id: 3, name: 'three'},
+            {id: 4, name: 'four'}
+        ],
         cart: []
     }
 });
