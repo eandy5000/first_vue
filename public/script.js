7d6e0aa948e91d35dc964455feacd4c8910d034a
Andy Eccleston <eandy5000@yahoo.com>
2017-04-17 11:02:10 -0500

    cart basics are working

--- public/script.js
+++ public/script.js
@@ -1,19 +1,33 @@
+var PRICE = 9.99;
+
 new Vue({
     el: '#app',
     methods: {
-        addToCart: function() {
-            console.log(cart)
-            return this.total += this.price;
+        addToCart: function(product) {
+            if(!product.qty || product.qty === 0) {
+                product.qty = 1
+                this.cart.push(product);
+                this.total += product.price;
+            } else {
+                product.qty++;
+                this.total += product.price;
+            }
+            
+        },
+        
+    },
+    filters: {
+        currency: function(num) {
+            return '$' + num.toFixed(2)
         }
     },
     data: {
         total: 0,
-        price: 9.99,
         products: [
-            {id: 1, name: 'one'},
-            {id: 2, name: 'two'},
-            {id: 3, name: 'three'},
-            {id: 4, name: 'four'}
+            {id: 0, name: 'one', price: 9.99 },
+            {id: 1, name: 'two', price: 5.99 },
+            {id: 2, name: 'three', price: 3.29 },
+            {id: 3, name: 'four', price: 11.18 }
         ],
         cart: []
     }
